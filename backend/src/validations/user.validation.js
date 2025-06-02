6f000d467c0131ebf4d72f73974b7756da07a01c
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-02 15:14:07 -0400

    Update user.validation.js

--- backend/src/validations/user.validation.js
+++ backend/src/validations/user.validation.js
@@ -2,9 +2,12 @@
 import Joi from "joi";
 
 const domainEmailValidator = (value, helper) => {
-  if (!value.endsWith("@gmail.cl")) {
+  if (
+    !value.endsWith("@gmail.cl") &&
+    !value.endsWith("@alumnos.ubiobio.cl")
+  ) {
     return helper.message(
-      "El correo electrónico debe ser del dominio @gmail.cl"
+      "El correo electrónico debe ser del dominio @gmail.cl o @alumnos.ubiobio.cl"
     );
   }
   return value;
@@ -26,7 +29,7 @@ export const userQueryValidation = Joi.object({
     .messages({
       "string.empty": "El correo electrónico no puede estar vacío.",
       "string.base": "El correo electrónico debe ser de tipo string.",
-      "string.email": "El correo electrónico debe finalizar en @gmail.cl.",
+      "string.email": "El correo electrónico debe finalizar en @gmail.cl o @alumnos.ubiobio.cl.",
       "string.min":
         "El correo electrónico debe tener como mínimo 15 caracteres.",
       "string.max":
@@ -73,7 +76,7 @@ export const userBodyValidation = Joi.object({
     .messages({
       "string.empty": "El correo electrónico no puede estar vacío.",
       "string.base": "El correo electrónico debe ser de tipo string.",
-      "string.email": "El correo electrónico debe finalizar en @gmail.cl.",
+      "string.email": "El correo electrónico debe finalizar en @gmail.cl o @alumnos.ubiobio.cl.",
       "string.min":
         "El correo electrónico debe tener como mínimo 15 caracteres.",
       "string.max":
