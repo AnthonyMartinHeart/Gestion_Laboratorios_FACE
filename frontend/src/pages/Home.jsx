d902c5a61c8883438d0bd3c9cacc244730d9d0da
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-05 02:34:00 -0400

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,19 +4,19 @@ import '@styles/Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <h5>Bienvenido al Sistema de Gestor de Laboratorios</h5>
-      <h2>Selecciona el laboratorio que se encuentre actualmente</h2>
-      <div className="lab-buttons">
-        <NavLink to="/select-pc/lab1">
-          <button className="lab-button">LAB 1</button>
-        </NavLink>
-        <NavLink to="/select-pc/lab2">
-          <button className="lab-button">LAB 2</button>
-        </NavLink>
-        <NavLink to="/select-pc/lab3">
-          <button className="lab-button">LAB 3</button>
-        </NavLink>
-      </div>
+      <h5>Bienvenido al Gestor de Laboratorios FACE</h5>
+      <h2>Seleccione Su Laboratorio 👇</h2>
+     <div className="lab-buttons">
+     <NavLink to="/select-pc/lab1">
+    <button className="lab-button lab1-button">LAB 1</button>
+   </NavLink>
+   <NavLink to="/select-pc/lab2">
+    <button className="lab-button lab2-button">LAB 2</button>
+   </NavLink>
+   <NavLink to="/select-pc/lab3">
+    <button className="lab-button lab3-button">LAB 3</button>
+  </NavLink>
+</div>
     </div>
   );
 };
