d062eba5be2543bfc593bee3a429d9ffcf7ab925
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-05 18:45:22 -0400

    Update Horarios.jsx

--- frontend/src/pages/Horarios.jsx
+++ frontend/src/pages/Horarios.jsx
@@ -1,12 +1,20 @@
 import HorarioLaboratorios from "@components/Horarios-Lab";
 
 export default function HorarioPage() {
+
   return (
+
     <div className="horario-container p-6">
-      <h2 className="hh text-3xl font-bold mb-6 text-center">
+
+      <hh className="hh text-3xl font-bold mb-6 text-center">
+
         Horario de Clases de los Laboratorios de Computación
-      </h2>
+      </hh>
+
       <HorarioLaboratorios />
+
     </div>
+
   );
+  
 }
