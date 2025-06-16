018dbd3534cd2050b648e43eb7a9e79e6d31c05a
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-15 22:44:31 -0400

    Update root.service.js

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 import cookies from 'js-cookie';
 
-<<<<<<< HEAD
 const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3006/api';
-=======
-const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3008/api';
->>>>>>> e7a17904b413b5f100201b433da5f612b375b052
 
 const instance = axios.create({
   baseURL: API_URL,
