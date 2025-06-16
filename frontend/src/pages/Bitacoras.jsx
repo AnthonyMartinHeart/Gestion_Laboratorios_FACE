c83a0a64e5e78dd08d6c40871f6c77eb685e486f
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-15 22:41:00 -0400

    Update Bitacoras.jsx

--- frontend/src/pages/Bitacoras.jsx
+++ frontend/src/pages/Bitacoras.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
-<<<<<<< HEAD
 import BitacoraTable from '@components/BitacoraTable';
 import { useGetAllReservations } from '@hooks/reservation/useGetAllReservations';
-=======
-import BitacoraTable, { exportToExcel } from '@components/BitacoraTable';
->>>>>>> e7a17904b413b5f100201b433da5f612b375b052
 import '@styles/bitacoras.css';
 
 const Bitacoras = () => {
   const [selectedDate, setSelectedDate] = useState('');
   const maxDate = '2026-12-31';
 
-<<<<<<< HEAD
   // Hooks para cada laboratorio
   const lab1 = useGetAllReservations(1, selectedDate);
   const lab2 = useGetAllReservations(2, selectedDate);
@@ -20,11 +15,6 @@ const Bitacoras = () => {
   const handleDateChange = (e) => setSelectedDate(e.target.value);
 
   const renderLaboratorio = (titulo, numEquipos, startIndex, labNumber, labData) => (
-=======
-  const handleDateChange = (e) => setSelectedDate(e.target.value);
-
-  const renderLaboratorio = (titulo, numEquipos, startIndex, labNumber) => (
->>>>>>> e7a17904b413b5f100201b433da5f612b375b052
     <div className="laboratorio-section" key={labNumber}>
       <div className="laboratorio-header">
         <h7>{titulo}</h7>
@@ -32,22 +22,9 @@ const Bitacoras = () => {
       <BitacoraTable
         numEquipos={numEquipos}
         startIndex={startIndex}
-<<<<<<< HEAD
         reservations={labData.reservations}
         date={selectedDate}
       />
-=======
-        date={selectedDate}
-      />
-      <div className="export-container">
-        <button
-          className="export-btn"
-          onClick={() => exportToExcel(numEquipos, startIndex, selectedDate)}
-        >
-          Exportar Bitácora {labNumber}
-        </button>
-      </div>
->>>>>>> e7a17904b413b5f100201b433da5f612b375b052
     </div>
   );
 
@@ -67,15 +44,9 @@ const Bitacoras = () => {
 
       {selectedDate && (
         <>
-<<<<<<< HEAD
           {renderLaboratorio('BITÁCORA LABORATORIO 1', 40, 1, 1, lab1)}
           {renderLaboratorio('BITÁCORA LABORATORIO 2', 20, 41, 2, lab2)}
           {renderLaboratorio('BITÁCORA LABORATORIO 3', 20, 61, 3, lab3)}
-=======
-          {renderLaboratorio('BITÁCORA LABORATORIO 1', 40, 1, 1)}
-          {renderLaboratorio('BITÁCORA LABORATORIO 2', 20, 41, 2)}
-          {renderLaboratorio('BITÁCORA LABORATORIO 3', 20, 61, 3)}
->>>>>>> e7a17904b413b5f100201b433da5f612b375b052
         </>
       )}
     </div>
