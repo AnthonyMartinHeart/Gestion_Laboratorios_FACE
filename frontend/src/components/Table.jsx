a02f6627e19fdc3f5369c0724af706fd3b65ef6f
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-06-24 14:47:39 -0400

    Update Table.jsx

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,7 +1,25 @@
 import useTable from '@hooks/table/useTable.jsx';
 
-export default function Table({ data, columns, filter, dataToFilter, initialSortName, onSelectionChange }) {
-  const { tableRef } = useTable({ data, columns, filter, dataToFilter, initialSortName, onSelectionChange });
+export default function Table({
+  data,
+  columns,
+  filter,
+  dataToFilter,
+  initialSortName,
+  onSelectionChange,
+  selectableRows = true,
+  selectedRows = [], 
+}) {
+  const { tableRef } = useTable({
+    data,
+    columns,
+    filter,
+    dataToFilter,
+    initialSortName,
+    onSelectionChange,
+    selectableRows,
+    selectedRows, 
+  });
 
   return (
     <div className='table-container'>
