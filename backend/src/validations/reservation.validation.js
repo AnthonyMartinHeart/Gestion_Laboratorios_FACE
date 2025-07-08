8919286b3b02e0821bc07630b6cf3df70f9fe4c8
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-07-07 21:54:10 -0400

    Update reservation.validation.js

--- backend/src/validations/reservation.validation.js
+++ backend/src/validations/reservation.validation.js
@@ -27,6 +27,7 @@ const horariosInicio = [
   "14:10",
   "15:40",
   "17:10",
+  "00:00", // Para mantenimiento
 ];
 
 const horariosTermino = [
@@ -38,13 +39,14 @@ const horariosTermino = [
   "17:00",
   "18:30",
   "20:00",
+  "23:59", // Para mantenimiento
 ];
 
 export const reservationValidation = Joi.object({
   rut: Joi.string()
     .min(9)
     .max(12)
-    .pattern(/^(?:(?:[1-9]\d{0}|[1-2]\d{1})(\.\d{3}){2}|[1-9]\d{6}|[1-2]\d{7}|29\.999\.999|29999999)-[\dkK]$/)
+    .pattern(/^(?:(?:[1-9]\d{0}|[1-2]\d{1})(\.\d{3}){2}|[1-9]\d{6}|[1-2]\d{7}|29\.999\.999|29999999|00\.000\.000)-[\dkK]$/)
     .required()
     .messages({
       "string.base": "El rut debe ser un texto.",
@@ -54,14 +56,14 @@ export const reservationValidation = Joi.object({
 
   carrera: Joi.string()
     .min(2)
-    .max(10)
+    .max(15)
     .pattern(/^[A-Z]+$/)
     .required()
     .messages({
       "string.base": "La carrera debe ser un texto.",
       "string.empty": "La carrera no puede estar vacía.",
       "string.min": "La carrera debe tener al menos 2 caracteres.",
-      "string.max": "La carrera no puede tener más de 10 caracteres.",
+      "string.max": "La carrera no puede tener más de 15 caracteres.",
       "string.pattern.base": "La carrera debe contener solo letras mayúsculas.",
     }),
 
