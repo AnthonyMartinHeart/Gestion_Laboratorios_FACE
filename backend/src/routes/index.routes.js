732b8d0597ef2aef335718b4b13a1f44f277f0db
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-07-20 00:41:10 -0400

    Update index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,12 +3,16 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import reservationRoutes from "./reservation.routes.js";
+import horarioRoutes from "./horario.routes.js";
+import estadisticasRoutes from "./estadisticas.routes.js";
 
 const router = Router();
 
 router
     .use("/auth", authRoutes)
     .use("/user", userRoutes)
-    .use("/reservas", reservationRoutes);
+    .use("/reservas", reservationRoutes)
+    .use("/horarios", horarioRoutes)
+    .use("/estadisticas", estadisticasRoutes);
 
 export default router;
