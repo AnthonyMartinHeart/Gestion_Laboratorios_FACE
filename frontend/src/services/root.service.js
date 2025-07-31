c02c6c5455604dbcec488a7d645738984cecccf2
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-07-31 17:30:52 -0400

    Update root.service.js

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import cookies from 'js-cookie';
 
-const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3040/api';
+const API_URL = import.meta.env.VITE_BASE_URL || 'http://10.4.3.76:80/api';
+//const API_URL = import.meta.env.VITE_BASE_URL || 'http://146.83.194.168:3040/api';
 
 console.log('API URL:', API_URL); // Esto ayuda a debuggear problemas de conexión
 
