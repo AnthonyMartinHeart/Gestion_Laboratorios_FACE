0737d2ef70f2ed0f15374b810f01994e376c52e9
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-08-06 00:55:46 -0400

    Update Turnos.jsx

--- frontend/src/pages/Turnos.jsx
+++ frontend/src/pages/Turnos.jsx
@@ -4,8 +4,12 @@ import '@styles/turnos.css';
 
 const Turnos = () => {
   const [selectedDate, setSelectedDate] = useState(() => {
+   
     const today = new Date();
-    return today.toISOString().split('T')[0];
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`; // Formato YYYY-MM-DD
   });
 
   return (
