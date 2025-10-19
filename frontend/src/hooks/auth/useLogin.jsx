de2ddaf69c148597b4b9a18cac9be94c723ce508
ℑ𝔯𝔬𝔫_𝔐𝔞𝔯𝔱𝔦𝔫ℌ𝔢𝔞𝔯𝔱 🎮🎯👾⚡❤‍🩹 <138081463+AnthonyMartinHeart@users.noreply.github.com>
2025-10-19 00:26:57 -0300

    Refactor useLogin hook to use 'rut' instead of 'email'

--- frontend/src/hooks/auth/useLogin.jsx
+++ frontend/src/hooks/auth/useLogin.jsx
@@ -1,32 +1,50 @@
 import { useState, useEffect } from 'react';
 
 const useLogin = () => {
-    const [errorEmail, setErrorEmail] = useState('');
+    const [errorRut, setErrorRut] = useState('');
     const [errorPassword, setErrorPassword] = useState('');
-    const [inputData, setInputData] = useState({ email: '', password: '' });
+    const [inputData, setInputData] = useState({ rut: '', password: '' });
+    const [prevRut, setPrevRut] = useState('');
 
     useEffect(() => {
-        if (inputData.email) setErrorEmail('');
+        if (inputData.rut) setErrorRut('');
         if (inputData.password) setErrorPassword('');
-    }, [inputData.email, inputData.password]);
+    }, [inputData.rut, inputData.password]);
 
     const errorData = (dataMessage) => {
-        if (dataMessage.dataInfo === 'email') {
-            setErrorEmail(dataMessage.message);
+        if (dataMessage.dataInfo === 'rut') {
+            setErrorRut(dataMessage.message);
         } else if (dataMessage.dataInfo === 'password') {
             setErrorPassword(dataMessage.message);
         }
     };
 
     const handleInputChange = (field, value) => {
-        setInputData(prevState => ({
-            ...prevState,
-            [field]: value
-        }));
+        if (field === 'rut') {
+            // Solo actualizar si el nuevo valor es válido o está vacío
+            if (value === '' || /^[0-9.kK-]*$/.test(value)) {
+                setPrevRut(value);
+                setInputData(prevState => ({
+                    ...prevState,
+                    [field]: value
+                }));
+            } else {
+                // Si el valor no es válido, mantener el valor anterior
+                setInputData(prevState => ({
+                    ...prevState,
+                    [field]: prevRut
+                }));
+            }
+        } else {
+            setInputData(prevState => ({
+                ...prevState,
+                [field]: value
+            }));
+        }
     };
 
     return {
-        errorEmail,
+        errorRut,
         errorPassword,
         inputData,
         errorData,
